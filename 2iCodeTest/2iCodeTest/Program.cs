b72a38d6681f7a279ee38de5fde11d9717ee101f
martin duffy <34552914+mduffy023@users.noreply.github.com>
2022-10-01 15:00:37 +0100

    Add files via upload

---
+++ 2iCodeTest/2iCodeTest/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//pre set int array list of 10 values
+int[] list = { 1, 5, 6, 8, 9, 8, 9, 7, 2, 3};
+
+//show the list unordered
+Console.WriteLine("here the list unordered");
+
+//foreachloop that take i and lis
+foreach (int i in list)
+{
+    //i + list 
+    Console.Write(i + " ");
+}
+
+//take any duplicates out of the array list 
+int[] dist = list.Distinct().ToArray();
+//keep the lis on he same line 
+Console.WriteLine("\nSorted List");
+//for loop that take the new list dist 
+for (int i = 0; i < dist.Length; i++)
+{
+    //sorts he array 
+    Array.Sort(dist);
+    Array.Reverse(dist);
+    //displays new array without the duplicates 
+    Console.Write(dist[i] + " ");
+
+}
+   
